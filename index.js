7895603f8e12a6f82e203d592219586e31bce66e
Philip osir Otieno <philiposir2@gmail.com>
2024-07-07 23:31:21 +0300

    update

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   let taskForm = document.addEventListener("submit", (e) =>{
     e.preventDefault();
     handleInput(e.target.inputArea.value);
+    let form = document.querySelector("#formContainer")
+    form.reset()
   })
 })
 
 function handleInput(shopList) {
   let p = document.createElement("p")
   p.style.fontSize = "20px";
+  p.style.fontStyle = "italic";
   p.style.fontWeight = "bold";
   let checkBox = document.createElement("input");
   checkBox.type="checkbox";
   checkBox.id = "myCheckbox";
   checkBox.style.marginLeft = "200px";
-  checkBox.style.padding = "50px"
+  checkBox.style.cursor = "pointer"
   checkBox.addEventListener("change", chekMark)
   p.textContent = `${shopList}`
   p.appendChild(checkBox)
